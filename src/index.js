2569dc931116d28334e039755dae19a4ba0146f4
angelo mbunga <a.mbunga@gmail.com>
2022-11-02 07:06:15 +0100

    code source

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './assets/fonts/Prata-Regular.ttf';
 import './assets/fonts/Archivo/Archivo-Bold.ttf';
 import './assets/fonts/Archivo/Archivo-SemiBold.ttf';
@@ -13,9 +13,9 @@ import './assets/fonts/Archivo/Archivo-Regular.ttf';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App/>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
